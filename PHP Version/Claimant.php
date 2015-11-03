a7931bcb2d4a9cb7d4b8eff6ad849666a2aa840f
Sandy Davidson <sandy@sandysandy.net>
2015-11-03 21:08:46 +0000

    PHP Version - Claimant class created

--- PHP Version/Claimant.php
+++ PHP Version/Claimant.php
@@ -1,10 +1,51 @@
 <?php
 
-class Claimant {
+class Claimant
+{
+  public $_age;
+  public $_sex;
+  public $_benefit;
+  public $_hoursCaring;
 
-  function __construct()
+  public function __construct($age, $sex, $benefits, $hoursCaring)
   {
-    # code goes here...
+      $this->_age = $age;
+      $this->_sex = $sex;
+      $this->_benefits = $benefits;
+      $this->_hoursCaring = $hoursCaring;
+  }
+
+  public function benefitScore() {
+     $score = 0;
+     $qualifyingBenefits = [
+      'Personal Independence Payment (PIP) daily living component',
+      'Disability Living Allowance (DLA) - the middle or highest care rate',
+      'Attendance Allowance',
+      'Constant Attendance Allowance normal maximum rate with an Industrial Injuries Disablement Benefit',
+      'Constant Attendance Allowance basic (full day) rate with a War Disablement Pension',
+      'Armed Forces Independence Payment (AFIP)'];
+
+
+    if($this->_age >= 16) $score++;
+    if($this->_age <= 65 && $this->_sex == 'male') $score++;
+    if($this->_age <= 63 && $this->_sex == 'female') $score++;
+    if($this->_hoursCaring >= 20) $score++;
+
+    for($i=0; $i < count($this->_benefits); $i++) {
+      if (in_array($this->_benefits[$i], $qualifyingBenefits)) $score++; break;
+    }
+
+    return $score;
+
+  }
+  public function entitlementCheck() {
+    $requiredScore = 4;
+
+    if($this->benefitScore() == $requiredScore) {
+      return 'yes';
+    } else {
+      return 'no';
+    }
   }
 }
 
